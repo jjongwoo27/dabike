61b84722b8f2c4c54c5eda893285ed85887ca89f
jjongwoo27 <leejongwoo97@gmail.com>
2022-11-29 19:28:29 +0900

    mod near_stations

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ async def root():
     return {"message": "2022-2 DA Senior Project DA-BIKE", "time": datetime.datetime.now()}
 
 
-@app.get("/location/")
-async def near_stations(lat: float = 37.5556488, lon: float = 126.9106293):
-    return dis.get_near_stations(lat, lon)
+@app.get("/api/location/")
+async def near_stations(latitude: float = 37.5556488, longitude: float = 126.9106293):
+    return dis.get_near_stations(latitude, longitude)
 
 
-@app.get("/recommendation/")
+@app.get("/api/recommendation/")
 async def recommended_stations(cafe: str = '1', food: str = '1', subway: str = '1', attraction: str = '1',
                                culture: str = '1', park: str = '1', humidity: int = 63, precipitation: float = 0.0,
                                temperature: float = 6.8, start_station: int = 102):
     comb = rec.get_combination(cafe, food, subway, attraction, culture, park, humidity, precipitation, temperature)
-
     return rec.get_recommended_stations(comb, start_station)
